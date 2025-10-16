5b1a0828abedaba763a6c2f7172de7e991841c32
derfoj <paulmouyebissi@gmail.com>
2025-10-16 18:55:34 +0200

    ajout des features dividing et multiplying

---
+++ features.py
@@ -0,0 +1,10 @@
+#files containing the different feature, multiplying and dividing 
+
+"""this is a functionthat mulpily two numbers"""
+def multiplying(a:int|float,b:int|float):
+    return a*b
+
+
+"""this is a functionthat divide two numbers"""
+def dividing(a:int|float,b:int|float):
+    return a/b
