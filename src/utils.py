eb49327c3159eb8fca28d5a8857642973f49f816
DesmondRane <206406920+DesmondRane@users.noreply.github.com>
2025-10-19 16:41:58 +0200

    implements add and substract functions

---
+++ src/utils.py
@@ -0,0 +1,16 @@
+"""implements add and subtract functions taking “unlimited” 
+number of params"""
+
+def add(*args):
+    return sum(args)
+
+def sub(*args):
+    """
+    Return the first arg minus all the following args.
+    """
+    diff = args[0]
+    for i in args[1:]:
+        diff -= i
+    return diff
+    
+ 
